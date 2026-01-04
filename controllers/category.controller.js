721ae4032e59337786572c3f5bc5c3f485dc8955
yousef mohamed <yousefmohamed17479191@gmail.com>
2026-01-04 13:06:55 +0200

    add Get Single Category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -8,9 +8,27 @@ const asyncHandler = require("express-async-handler");
 // @route               GET /api/v1/category
 // @access              Public
 exports.getCategory = asyncHandler(async (req, res) => {
-  const categories = await categoryModel.find({});
-  res.status(200).json({ result: categories.length, data: categories });
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  const categories = await categoryModel.find({}).skip(skip).limit(limit);
+  res.status(200).json({ page: page, result: categories.length, data: categories });
 });
+
+
+// @desc                Get single category
+// @route               GET /api/v1/category/:id
+// @access              Public
+exports.getSingleCategory= asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  const category = await categoryModel.findById(id);
+  if (!category) {
+    res.status(404).json({ msg: `Category not found for id ${id}` });
+  }
+
+  res.status(200).json({ data: category });
+});
+
 
 // @desc                Create new category
 // @route               POST /api/v1/category
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const { getCategory,createCategory } = require("../controllers/category.controller");
+const {
+  getCategory,
+  getSingleCategory,
+  createCategory,
+} = require("../controllers/category.controller");
 const router = express.Router();
 
 router.route("/").get(getCategory).post(createCategory);
-
+router.route("/:id").get(getSingleCategory);
 module.exports = router;
