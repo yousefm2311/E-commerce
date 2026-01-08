1b66d0e10bbec3cb6e7cddcba709491505e0bde4
yousef mohamed <yousefmohamed17479191@gmail.com>
2026-01-08 12:33:11 +0200

    add validator in category

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const {
+  getCategoryValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidator");
+const {
   getCategory,
   getSingleCategory,
   createCategory,
   updateCategory,
-  deleteCategory
+  deleteCategory,
 } = require("../controllers/category.controller");
 const router = express.Router();
 
-router.route("/").get(getCategory).post(createCategory);
-router.route("/:id").get(getSingleCategory).put(updateCategory).delete(deleteCategory); 
+router
+  .route("/")
+  .get(getCategory)
+  .post(createCategoryValidator, createCategory);
+router
+  .route("/:id")
+  .get(getCategoryValidator, getSingleCategory)
+  .put(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
 module.exports = router;
---
+++ utils/validators/categoryValidator.js
@@ -0,0 +1,34 @@
+const { check } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+
+exports.getCategoryValidator = [
+  check("id").isMongoId().withMessage("Invalid Category ID format "),
+  validatorMiddleware,
+];
+
+exports.createCategoryValidator = [
+  check("name")
+    .notEmpty()
+    .withMessage("Category name is required")
+    .isLength({ min: 3 })
+    .withMessage("Category name must be at least 3 characters")
+    .isLength({ max: 32 })
+    .withMessage("Category name must be at most 32 characters"),
+  validatorMiddleware,
+];
+exports.updateCategoryValidator = [
+  check("id").isMongoId().withMessage("Invalid Category ID format "),
+  check("name")
+    .notEmpty()
+    .withMessage("Category name is required")
+    .isLength({ min: 3 })
+    .withMessage("Category name must be at least 3 characters")
+    .isLength({ max: 32 })
+    .withMessage("Category name must be at most 32 characters"),
+  validatorMiddleware,
+];
+
+exports.deleteCategoryValidator = [
+  check("id").isMongoId().withMessage("Invalid Category ID format "),
+  validatorMiddleware,
+];
