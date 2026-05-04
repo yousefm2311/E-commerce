6051a43bf97b9bfd3e98cd2a861947ec899564ef
yousef mohamed <yousefmohamed17479191@gmail.com>
2026-05-04 11:29:37 +0300

    add seeder

--- utils/dummyData/seeder.js
+++ utils/dummyData/seeder.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 require("colors");
 const dotenv = require("dotenv");
-const Product = require("../../models/productModel");
-const dbConnection = require("../../config/database");
+const Product = require("../../models/productModel.js");
+const dbConnection = require("../../config/database.js");
 
-dotenv.config({ path: "../../config.env" });
+dotenv.config({ path: "../../.env" });
 
 // connect to DB
 dbConnection();
