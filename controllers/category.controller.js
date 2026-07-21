65b418356f76827e1d0cacbebeffb8d8848713e6
yousef mohamed <yousefmohamed17479191@gmail.com>
2026-07-21 13:36:26 +0300

    add validate in images

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,27 +1,36 @@
 const categoryModel = require("../models/categoryModel");
-const factory = require('./handlersFactory.js');
+const ApiError = require("../utils/apiErrors.js");
+const factory = require("./handlersFactory.js");
 const multer = require("multer");
-const {v4:uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 // disk storage middleware
 const multerStorage = multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,'uploads/categories');
-    },
-    filename: function(req,file,cb){
-        const ext = file.mimetype.split('/')[1];
-        const fileName = `category-${uuidv4()}-${Date.now()}.${ext}`;
-        cb(null,fileName);
-    }
+  destination: function (req, file, cb) {
+    cb(null, "uploads/categories");
+  },
+  filename: function (req, file, cb) {
+    const ext = file.mimetype.split("/")[1];
+    const fileName = `category-${uuidv4()}-${Date.now()}.${ext}`;
+    cb(null, fileName);
+  },
 });
 
-const upload = multer({ storage: multerStorage });
+const multerFilter = function (req, file, cb) {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new ApiError("Onlys Image allowed", 400), false);
+  }
+};
 
-exports.updateCategoryImage = upload.single("image")
+const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
+
+exports.updateCategoryImage = upload.single("image");
 // @desc                Get all categories
 // @route               GET /api/v1/category
 // @access              Public
-exports.getCategory = factory.getAll(categoryModel,"Category");
+exports.getCategory = factory.getAll(categoryModel, "Category");
 
 // @desc                Get single category
 // @route               GET /api/v1/category/:id
